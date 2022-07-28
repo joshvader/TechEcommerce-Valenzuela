236083f8c80666775f0e81dbd70d73e5da1280d3
proyect_icaro <josuevalenzuela87@gmail.com>
2022-07-28 16:37:35 -0400

    Modificación en Product List y uso de useEffects

---
+++ src/Utils/ProductList.js
@@ -0,0 +1,20 @@
+export const ProductList = [
+    {
+      id: 1,
+      name: "Linux",
+      role: "Operating system",
+      img: "https://codealo-commerce-cms.onrender.com/uploads/linux_48923655d6.png",
+    },
+    {
+      id: 2,
+      name: "Nuxt.js",
+      role: "Intuitive Vue Framework",
+      img: "https://codealo-commerce-cms.onrender.com/uploads/nuxt_js_948b22e2df.png",
+    },
+    {
+      id: 3,
+      name: "React",
+      role: "Library",
+      img: "https://codealo-commerce-cms.onrender.com/uploads/react_496bc591f8.png",
+    },
+  ]
--- src/components/Productos/ItemListContainer.jsx
+++ src/components/Productos/ItemListContainer.jsx
@@ -1,33 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { ProductList } from '../../Utils/ProductList'; 
 import Item from './ItemList';
 const ItemListContainer = () => {
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: "Linux",
-      role: "Operating system",
-      img: "https://codealo-commerce-cms.onrender.com/uploads/linux_48923655d6.png",
-    },
-    {
-      id: 2,
-      name: "Nuxt.js",
-      role: "Intuitive Vue Framework",
-      img: "https://codealo-commerce-cms.onrender.com/uploads/nuxt_js_948b22e2df.png",
-    },
-    {
-      id: 3,
-      name: "React",
-      role: "Library",
-      img: "https://codealo-commerce-cms.onrender.com/uploads/react_496bc591f8.png",
-    },
-  ]
+  const [products, setProducts] = useState([]);
   
- );
+
  const mPromesa = new Promise((resolve, reject) => {
   setTimeout(() =>{
-    res (product);
+    resolve (ProductList);
   }, 2000)
 })
+useEffect(()=>{
+  mPromesa
+  .then((resolve)=> setProducts(resolve))
+},[])
 
 
 
